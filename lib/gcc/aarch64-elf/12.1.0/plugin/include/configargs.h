3b7324de9746addb9812113fe45fb647a0ff77a7
toss-a <654715610@qq.com>
2022-08-05 23:16:03 +0800

    Add aarch64-elf GCC 12.1.0 toolchain for Linux
    Binutils Branch: master
    GCC Branch: releases/gcc-12.1.0
    LLVM HEAD: llvm/llvm-project@501faaa
    Compiler Optimizations: -O3 -flto
    
    Built in Ubuntu 22.04 LTS (GNU/Linux 5.15.0-43-generic x86_64)
    
    Co-authored-by: GarfieldHan <2652609017@qq.com>
    Signed-off-by: toss-a <654715610@qq.com>

---
+++ lib/gcc/aarch64-elf/12.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf CFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/media/toss/SSD/gcc_ws/gcc-build/../gcc-arm64 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='GarfieldHan GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot --with-headers=/usr/include";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
